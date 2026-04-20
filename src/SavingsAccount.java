c4ee1a60077faf93925ef5bba23fbef7c695db09
Raissa427 <numubyeyiirumvaraissa@gmail.com>
2026-04-20 23:07:38 +0200

    Add SavingsAccount class extending Account

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+public class SavingsAccount extends Account {
 
-public class SavingsAccount {
+    public SavingsAccount(double balance) {
+        super(balance);
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+    }
 }

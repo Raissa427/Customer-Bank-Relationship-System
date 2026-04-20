de627398fbaf3110a59664aaaf4c7c80abdc9eb3
Raissa427 <numubyeyiirumvaraissa@gmail.com>
2026-04-20 23:08:03 +0200

    Add main class to test system functionality

--- src/Bank.java
+++ src/Bank.java
@@ -1,5 +1,18 @@
-public class Main {
+public class Bank {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+            Account savings = new SavingsAccount(1000);
+            Account current = new CurrentAccount(500);
+
+            Customer c1 = new Customer("Raissa", savings);
+            Customer c2 = new Customer("John", current);
+
+            c1.getAccount().deposit(200);
+            c1.getAccount().withdraw(150);
+
+            c2.getAccount().withdraw(600);
+
+            c1.displayInfo();
+            c2.displayInfo();
+        }
     }
-}

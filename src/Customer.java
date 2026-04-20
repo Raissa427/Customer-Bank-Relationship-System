42223b895ded1add92b5e256fa5eb224ab9bc094
Raissa427 <numubyeyiirumvaraissa@gmail.com>
2026-04-20 23:07:54 +0200

    Create Customer class with encapsulation and account relationship

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Customer {
+    private String name;
+    private Account account;
+
+    public Customer(String name, Account account) {
+        this.name = name;
+        this.account = account;
+    }
+
+    public void displayInfo() {
+        System.out.println("Customer: " + name);
+        System.out.println("Balance: " + account.getBalance());
+    }
+
+    public Account getAccount() {
+        return account;
+    }
 }

24740cb0baa3ce94d4148b73bf460e76298f2688
Raissa427 <numubyeyiirumvaraissa@gmail.com>
2026-04-20 23:07:47 +0200

    Add CurrentAccount class with overdraft behavior

--- src/CurrentAccount.java
+++ src/CurrentAccount.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+public class CurrentAccount extends Account {
 
-public class CurrentAccount {
+    public CurrentAccount(double balance) {
+        super(balance);
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        balance -= amount; // allows overdraft
+        System.out.println("Withdrawn (overdraft allowed): " + amount);
+    }
 }

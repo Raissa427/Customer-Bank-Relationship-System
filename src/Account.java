4c6053a7de1598c1214652ae8541cef2b7e3bd69
Raissa427 <numubyeyiirumvaraissa@gmail.com>
2026-04-20 23:07:16 +0200

    Create abstract Account class with balance and methods

---
+++ src/Account.java
@@ -0,0 +1,19 @@
+public class Account {
+    protected double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+
+    }
+
+    public void withdraw(double amount) {
+
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
